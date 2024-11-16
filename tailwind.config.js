96ab78b37526335c8f9c88b3ddcfde4abf4b661a
1AlexDev <157933564+1AlexDev@users.noreply.github.com>
2024-11-16 12:31:27 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './public/**/*.{html,js,ts,jsx,tsx}',  // Optional if you’re using Tailwind in public files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
